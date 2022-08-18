dff37608f4b757e92c9b0c6033635eb617ab9455
Riddick <riddick.angiela@student.moringaschool.com>
2022-08-18 14:20:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(fun="roller-skate"){
+    return(`This Saturday, I want to ${fun}!`);
+
+  }
+  saturdayFun();
+  const  mondayWork=function(work="go to the office"){
+    return`This Monday, I will ${work}.`;
+  }
+  saturdayFun("from home");
+
+  function wrapAdjective(adjective="*"){
+    return function(name="special"){
+            return(`You are ${adjective}${name}${adjective}!`);
+          };
+    }
+console.log(wrapAdjective("%") ("a dedicated coder"))
